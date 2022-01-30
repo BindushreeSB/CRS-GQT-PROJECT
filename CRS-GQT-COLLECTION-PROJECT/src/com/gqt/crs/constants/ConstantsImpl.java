f59b7c7eb032e5be403f86d4d2fa53a76c522fb7
BindushreeSB <bindushreesb01@gmail.com>
2022-01-30 19:17:40 +0530

    Multiple Package Addition

---
+++ CRS-GQT-COLLECTION-PROJECT/src/com/gqt/crs/constants/ConstantsImpl.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.gqt.crs.constants;
+
+/**
+ * @author Bindu
+ *
+ */
+public class ConstantsImpl {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
